babce4da3d8bd302c4df56e7a8c2e96a306f58cc
Vo Huy Hung <22520528@gm.uit.edu.vn>
2024-10-24 20:22:48 +0700

    set view engine

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const ejs = require("ejs");
 const app = express();
 
 app.use(express.json());
@@ -7,6 +8,12 @@ const __dirNameList = __dirname.split("\\");
 const __rootDir = __dirNameList.slice(0, -1).join("\\");
 const __viewsDir = __rootDir + "\\frontend\\views";
 const __publicDir = __rootDir + "\\frontend\\public";
+
+app.use(express.static(__publicDir));
+
+app.set("views", __viewsDir);
+app.set("view engine", "ejs");
+
 console.log(__publicDir);
 app.listen(3000, () => {
   console.log("server running on 3000");
