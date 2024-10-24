b7f69dcf9c496f76086debe261e795bcbfdab63b
Vo Huy Hung <22520528@gm.uit.edu.vn>
2024-10-24 20:18:53 +0700

    set view public and root dir

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,11 @@ const app = express();
 
 app.use(express.json());
 
+const __dirNameList = __dirname.split("\\");
+const __rootDir = __dirNameList.slice(0, -1).join("\\");
+const __viewsDir = __rootDir + "\\frontend\\views";
+const __publicDir = __rootDir + "\\frontend\\public";
+console.log(__publicDir);
 app.listen(3000, () => {
-  console("server running on 3000");
+  console.log("server running on 3000");
 });
