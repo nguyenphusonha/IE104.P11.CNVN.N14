f6a7301686bc0ae4709999edac24c877987134e6
Vo Huy Hung <22520528@gm.uit.edu.vn>
2024-12-06 21:51:15 +0700

    add product patch method

--- backend/routers/products.router.js
+++ backend/routers/products.router.js
@@ -2,36 +2,13 @@ const express = require("express");
 const router = express.Router();
 const Product = require("../models/products.model");
 const Category = require("../models/category.model");
+const { default: mongoose } = require("mongoose");
 router.get("/", async (req, res) => {
   const productList = await Product.find().populate("category");
   if (!productList) {
     res.status(500).json({ success: false });
   }
   res.send(productList);
-});
-router.get("/:id", async (req, res) => {
-  const product = await Product.findById(req.params.id).populate("category");
-  if (!product) {
-    res.status(500).json({ success: false });
-  }
-  res.send(product);
-});
-router.delete("/:id", (req, res) => {
-  Product.findByIdAndDelete(req.params.id)
-    .then((product) => {
-      if (product) {
-        return res
-          .status(200)
-          .json({ sucees: true, message: "The product was deteted" });
-      } else {
-        return res
-          .status(404)
-          .json({ sucees: false, message: "No product with id" });
-      }
-    })
-    .catch((err) => {
-      return res.status(400).json({ sucees: false, error: err });   
-    });
 });
 router.post("/", async (req, res) => {
   const category = await Category.findById(req.body.category);
@@ -51,4 +28,52 @@ router.post("/", async (req, res) => {
   }
   res.send(product);
 });
+router.get("/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id).populate("category");
+  if (!product) {
+    res.status(500).json({ success: false });
+  }
+  res.send(product);
+});
+router.delete("/:id", (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send("Invalid product id");
+  }
+  Product.findByIdAndDelete(req.params.id)
+    .then((product) => {
+      if (product) {
+        return res
+          .status(200)
+          .json({ sucees: true, message: "The product was deteted" });
+      } else {
+        return res
+          .status(404)
+          .json({ sucees: false, message: "No product with id" });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({ sucees: false, error: err });
+    });
+});
+
+//PATCH
+router.patch("/:id", async (req, res) => {
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    res.status(400).send("Invalid product id");
+  }
+  if (req.body.category) {
+    const category = await Category.findById(req.body.category);
+    if (!category) return res.status(400).send("Invalid category");
+  }
+  const updates = { ...req.body };
+  const updatedProduct = await Product.findByIdAndUpdate(
+    req.params.id,
+    updates,
+    { new: true, runValidators: true }
+  ).catch((err) => {
+    return res.status(400).json({ sucees: false, error: err });
+  });
+  res.status(200).send(updatedProduct);
+});
+
 module.exports = router;
